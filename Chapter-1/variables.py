58b8517f1621c4404544e279899ee2bb591c9ce9
ABHISHEK-RAI007 <abhishek.rai0522@gmail.com>
2023-12-24 10:28:03 +0530

    Variables & Basic Data type in Python

---
+++ Chapter-1/variables.py
@@ -0,0 +1,44 @@
+# String
+name = "Abhishek"
+# print(type(name))
+
+#integer
+roll_number = 17
+print(type(roll_number))
+
+rollNumber = 18
+
+# floating numer
+percentage = 95.8
+# print(type(percentage))
+
+#boolean 
+isStudent = True
+is_Student = True
+# print(type(is_Student))
+
+# print (name)
+# print(roll_number)
+# print(is_Student)
+# print(name , roll_number, percentage, is_Student)
+
+# percentage = 94
+
+# print(name , roll_number, percentage, is_Student)
+
+
+print("My name is " + name + " and my role number is", roll_number)
+
+print("I scored", percentage, "% in my final exams. I am a student is", is_Student)
+
+# print expressions 
+print("My percentage has changes to", percentage - 1)
+
+# printing with separators
+print(name , roll_number, percentage, is_Student, sep="-")
+
+
+x = 1
+y = 2
+z = 3
+print(x, y, z, sep="<-")
