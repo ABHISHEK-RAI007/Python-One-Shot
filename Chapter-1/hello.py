1973b8c75e41aa6b87a2a4c87602af303453e534
ABHISHEK-RAI007 <abhishek.rai0522@gmail.com>
2023-12-24 10:09:03 +0530

    Basics of Python

---
+++ Chapter-1/hello.py
@@ -0,0 +1,15 @@
+print("Hello World!, This is Urvi")
+
+# How to move to next line? 
+print("Hello, \n World")
+
+# Comments in Python 
+# print("Hello, \n World")
+
+'''print("Statement2")
+print("Statement3") 
+'''
+# Python Indentation
+    #  print("Hello World") #Remvoe "#" to check IndentationError.
+
+# Python CLI 
