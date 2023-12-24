7a0746bd1a3dea74dbe0ce6d2ad0766703216f1a
ABHISHEK-RAI007 <abhishek.rai0522@gmail.com>
2023-12-24 20:47:47 +0530

    ASCII Values

---
+++ Chapter-1/acii.py
@@ -0,0 +1,23 @@
+char = "a"
+print(ord(char))
+
+char = "b"
+print(ord(char))
+
+char = "A"
+print(ord(char))
+
+char = " "
+print(ord(char))
+
+ascii = 67
+print(chr(ascii))
+
+ascii = 32
+print(chr(ascii))
+
+ascii = 98
+print(chr(ascii))
+
+ascii = 97
+print(chr(ascii))
