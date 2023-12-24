3338c7dfcf000e18ab0de5072074901f381e0f05
ABHISHEK-RAI007 <abhishek.rai0522@gmail.com>
2023-12-25 00:06:16 +0530

    Input Prompt

---
+++ Chapter-1/input.py
@@ -0,0 +1,16 @@
+# Taking Input  
+
+# name = input("Enter your name: ")
+# print(name)
+
+# Type casting 
+# age = int(input("Enter your age:"))
+# print(type(age))
+
+# Write a program to find the sum of two numbers entered by the user.  
+
+x = int(input("Enter x number: "))
+y = int(input("Enter y number: "))
+
+sum = x + y
+print("The sum of these two numbers is", sum)
