80773b05b5c5bd8ef707259451e374729f07053a
ABHISHEK-RAI007 <abhishek.rai0522@gmail.com>
2023-12-26 12:24:57 +0530

    Type of Operators

---
+++ Chapter-1/operators.py
@@ -0,0 +1,89 @@
+# Arithmetic Operators
+# print("sum:", 4 + 3)
+# print("difference:", 4 - 3)
+# print("product:", 4 * 3)
+# print("division:", 4 / 3)
+# print("floor division:", 4 // 3)
+# print("remainder:", 4 % 3)
+# print("exponentiation:", 4**3)
+
+# Assignment Operators
+# n1 = 5
+# n2 = n1
+# print(n1, n2)
+
+# n2 += n1
+# print(n1, n2)
+
+# n2 -= n1
+# print(n1, n2)
+
+# n2 *= n1
+# print(n1, n2)
+
+# Comparison Operators
+# n1 = 4
+# n2 = 2
+
+# # Equal To
+# print(n1 == n2)
+
+# # Not Equal To
+# print(n1 != n2)
+
+# # Greater Than
+# print(n1 > n2)
+# print(n1 >= n2)
+
+# #Less Than
+# print(n1 < n2)
+# print(n1 <= n2)
+
+
+#Logical Operators
+
+Exp1 = 2 > 1
+Exp2 = 5 < 4
+
+# # and Operators 
+# print("Ex1 and Exp2:", Exp1 and Exp2)
+
+# # Or Operators 
+# print("Ex1 or Exp2:", Exp1 or Exp2)
+
+# # Not Operators 
+# print("Exp1:", not(Exp1))
+
+
+# identity operators
+x = 5
+y = 5
+# print("if x is y:", x is y)
+# print("if x is not y:", x is not y)
+
+# Membership Operators
+# fruits = ["apple", "banana", "cherry"]
+# print("if banana is present in fruits:", "banana" in fruits)
+# print("if banana is not present in fruits:", "mango" not in fruits)
+
+# Bitwise Operators
+a = 5
+b = 3
+# print("a & b :", a & b)
+# print("a or b :", a | b)
+# print("a xor b :", a ^ b)
+
+# Operator Precedence 
+# 1. () 
+# 2. ** 
+# 3. /, //, % 
+# 4. * 
+# 5. +
+# 6. -
+# 7. Bitwise Shift >>, <<
+# 8. &, |, ^
+# 8. comperison Operation => ==, !=, >, <
+# 8. Logical Operation =>  not, and, or
+
+Op  = 3 + 2 **4 / 2 * 5 -8 // 2
+print(Op)
