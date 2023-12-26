392396860d0e7c7321559db89657395bf94f5eb9
ABHISHEK-RAI007 <abhishek.rai0522@gmail.com>
2023-12-26 12:25:27 +0530

    Assignment

---
+++ Chapter-1/sphereVolume.py
@@ -0,0 +1,8 @@
+#taking radius as input 
+radius = float(input("Enter radius:"))
+
+# Calculate Volume
+volume = (4 / 3) * 3.1415 * (radius ** 3)
+
+# print the volume of sphere
+print("Volume of sphere is :", volume)
