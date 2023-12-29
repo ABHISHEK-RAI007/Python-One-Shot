1b395a9b25f4b899611df4dc6ca9e189e0476862
ABHISHEK-RAI007 <abhishek.rai0522@gmail.com>
2023-12-29 23:58:01 +0530

    Assignment

---
+++ Chapter-1/celsiusToFah.py
@@ -0,0 +1,9 @@
+# f  = c * (9 / 5) + 32
+
+# taking temprature in celsius as input
+celsius_temp = float(input("Enter temperature in celsius:"))
+
+# converting it to F
+fah_temp = (celsius_temp * 9 /5) + 32
+
+print("Temperature in fahrenheit:", fah_temp)
