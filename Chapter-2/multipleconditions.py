266f1fd1b2e767d0f5b6262dbbb080a2ecbb8c5e
ABHISHEK-RAI007 <abhishek.rai0522@gmail.com>
2023-12-31 09:15:17 +0530

    Multiple Conditions Using ‘and’ and ‘or’

---
+++ Chapter-2/multipleconditions.py
@@ -0,0 +1,12 @@
+eng_marks = int(input("Enter marks in English: "))
+math_marks = int(input("Enter marks in Math: "))
+
+# if both are more than 80, print A grade
+if (eng_marks > 80 and math_marks > 80) :
+    print("A Grade")
+# if either of marks are more than 80, print B grade
+elif (eng_marks > 80 or math_marks > 80):
+    print("B Grade")
+# if neither of marks are more than 80
+else:
+    print("C Grade")
