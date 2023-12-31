cf5b4d5391ca65b74b1e11b009e4d73e4275854a
ABHISHEK-RAI007 <abhishek.rai0522@gmail.com>
2023-12-31 12:39:28 +0530

    Multiple Conditions Using ‘and’

---
+++ Chapter-2/greatest.py
@@ -0,0 +1,13 @@
+n1 = int(input("Enter number1:"))
+n2 = int(input("Enter number2:"))
+n3 = int(input("Enter number3:"))
+
+# if n1 is greatest
+if (n1 > n2 and n1 > n3):
+    print(n1, "n1 is the greatest number"),
+# if n2 is the greatest 
+elif (n2 > n1 and n2 > n3 ):
+    print(n2, "n2 is the greatest number"),
+# if n3 is the greatest 
+else:
+    print(n3, "n3 is the greatest number"),
