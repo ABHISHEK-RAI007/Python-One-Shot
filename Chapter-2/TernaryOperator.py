3cb5565824d39c3631983e8dc88c494d6826367f
ABHISHEK-RAI007 <abhishek.rai0522@gmail.com>
2024-01-01 12:04:32 +0530

    Ternary Operator

---
+++ Chapter-2/TernaryOperator.py
@@ -0,0 +1,8 @@
+num = int(input("Enter the number:"))
+
+#   Method -1
+output = "Even" if (num % 2 == 0) else ("Odd")
+print("Output is", output)
+
+#   Method -25
+print("Output is", "Even" if (num % 2 == 0) else ("Odd"))
