f83ec8e23db5398b49fdf7d0bf70a109ecf05967
ABHISHEK-RAI007 <abhishek.rai0522@gmail.com>
2024-01-02 18:00:48 +0530

    For Loop

---
+++ Chapter-2/forLoop.py
@@ -0,0 +1,11 @@
+for i in range(1, 11):
+    print(i,"Hello world")
+
+for i in range(1, 11, 2):
+    print(i,"Hello world")
+
+for i in range(11):
+    print(i,"Hello world")
+
+for _ in range(10):
+    print(i,"Hello world")
