62a263f7c0ae1271fd49765b8a3573e733d43746
ABHISHEK-RAI007 <abhishek.rai0522@gmail.com>
2024-01-03 20:37:58 +0530

    list

---
+++ Chapter-3/list.py
@@ -0,0 +1,15 @@
+fruits = ["apple", "banana", "cherry"] 
+print(fruits)
+
+# checking type of list
+print(type(fruits)) 
+
+# checking length of list
+print(len(fruits))
+
+# checking if an item is present in the list
+if "banana" in fruits:
+    print("Banana is part of the list")
+
+if "kiwi" not in fruits:
+    print("Kiwi is not part of the list")
