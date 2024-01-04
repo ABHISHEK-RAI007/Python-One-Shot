c09173bf018bd3435d0e3c12e3a5cd8431f177be
ABHISHEK-RAI007 <abhishek.rai0522@gmail.com>
2024-01-04 10:22:48 +0530

    changing/updating item in a list

--- Chapter-3/list.py
+++ Chapter-3/list.py
@@ -44,3 +44,14 @@ print(fruits)
 # fruits.pop()
 # print(fruits)
 
+
+#  changing/updating item in a list
+
+# fruits[1] =  "kiwi"
+# print(fruits)
+
+# fruits[1:3] = ["papaya", ""]
+# print(fruits)
+
+# fruits[1:3] = "papaya"
+# print(fruits)
