2b3af9cda58b02b16d32c4200c842a48f42154c4
ABHISHEK-RAI007 <abhishek.rai0522@gmail.com>
2024-01-09 10:04:10 +0530

    copy a list

--- Chapter-3/list.py
+++ Chapter-3/list.py
@@ -70,12 +70,12 @@ print(fruits)
 # print(fruits)
 
 #  list comprehension
-new_fruits = [fruit for fruit in fruits if "a" in fruit]
+# new_fruits = [fruit for fruit in fruits if "a" in fruit]
+# print(new_fruits)
+
+# copy a list
+new_fruits = fruits.copy()
 print(new_fruits)
 
-# #  copy a list
-# new_fruits = fruits.copy()
-# print(new_fruits)
-
-# new_fruits =  fruits + new_fruits
-# print(new_fruits)
+new_fruits =  fruits + new_fruits
+print(new_fruits)
