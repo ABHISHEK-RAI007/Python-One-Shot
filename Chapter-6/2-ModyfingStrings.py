a3cd94c47ae8ed4d80a7c599e75507e61a40b008
ABHISHEK-RAI007 <abhishek.rai0522@gmail.com>
2024-02-09 19:46:35 +0530

    Modyfing Strings

--- Chapter-6/2-ModyfingStrings.py
+++ Chapter-6/2-ModyfingStrings.py
@@ -17,3 +17,40 @@ str1 = "    Hello World!  "
 print(str1.strip())
 print(str1)
 
+#  Replace substring
+str1 = "Hello World, what a beutiful world this is "
+print(str1.replace("World", "Earth"))
+print(str1.replace("World", "Earth", 1))
+
+#  Splitting string
+str1 = "Ria Pia Sia Tia Mia"
+list = str1.split()
+print(list)
+
+str1 = "Ria, Pia, Sia, Tia, Mia"
+list = str1.split(",", 2)
+
+#  Concatenation of strings
+str1 = "Hello World!"
+str2 = "what a beutiful world this is"
+print(str1 + " " + str2)
+
+#  String Formatting 
+student_name  = "Pallavi"
+student_marks  = 98
+
+str1 = "The student name is {s} and marks is {m}". format(s = student_name, m = student_marks)
+print(str1)
+
+
+text = "The unexpected always happens "
+print(text)
+print(len(text))
+print(text.find("pp"))
+print(text[:11])
+print(text.replace('always', 'never'))
+text2 = "no matter whats"
+new_text = text + text2
+print(new_text)
+
+
