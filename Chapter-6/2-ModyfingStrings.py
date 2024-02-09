3ac1b5caa35daf7eb8a1edc7a050c894a328bf05
ABHISHEK-RAI007 <abhishek.rai0522@gmail.com>
2024-02-09 18:55:52 +0530

    Modyfing Strings

---
+++ Chapter-6/2-ModyfingStrings.py
@@ -0,0 +1,21 @@
+# Modyfing Strings
+#  for converting characters to uppercase
+str1 = "New York"
+str2 = str1.upper()
+print(str2)
+
+#  for converting characters to lowercase
+str3 = str2.lower()
+print(str3)
+
+#  For capitalising the first character of my string 
+str4 = str3.capitalize()
+print(str4)
+
+#  for stripping/removing white spaces from the beginning and end of a string characters
+str1 = "    Hello World!  "
+print(str1.strip())
+print(str1)
+
+
+
