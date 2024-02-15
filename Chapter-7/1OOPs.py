a21230109fc818e0653ec26ab9f3e0311f0b4f6d
ABHISHEK-RAI007 <abhishek.rai0522@gmail.com>
2024-02-15 17:42:26 +0530

    Update OOP

--- Chapter-7/1OOPs.py
+++ Chapter-7/1OOPs.py
@@ -2,7 +2,7 @@
 class Student:
     
     def setName(self, name):
-        self.name = name
+        self.name = name # Class Attributes 
 
     def getName(self):
         return self.name
@@ -11,6 +11,10 @@ student1 = Student()
 student1.setName("Shehnaaz")
 print(student1.name)
 print(student1.getName())
+
+# instance attribute
+student1.eng_marks = 45  
+print(student1.eng_marks)
 
 student2 = Student()
 student2.setName("Parag")
